1117e0aae96f245f790dd1507ce02779551fe53d
pawan <justpawan7@gmail.com>
2025-11-15 18:27:55 -0600

    Word Jumble: Chapter 3

---
+++ World Jumble/World Jumble/World Jumble.cpp
@@ -0,0 +1,74 @@
+// World Jumble
+// The classic world jumble game where the player can ask for a hint
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    enum fields { WORD, HINT, NUM_FIELDS };
+    const int NUM_WORDS = 5;
+    const string WORDS[NUM_WORDS][NUM_FIELDS] =
+    {
+        {"wall", "Do you feel you’re banging your head against something?"},
+        {"glasses", "These might help you see the answer."},
+        {"labored", "Going slowly, is it?"},
+        {"persistent", "keep at it"},
+        {"jumble", "It's what the game is all about"},
+    };
+
+    srand(static_cast <unsigned int> (time(0)));
+    int choice = (rand() % NUM_WORDS);
+    string theWord = WORDS[choice][WORD];
+    string theHint = WORDS[choice][HINT];
+
+    string jumble = theWord;
+    int length = jumble.size();
+    for (int i = 0; i < length; ++i)
+    {
+        int index1 = (rand() % length);
+        int index2 = (rand() % length);
+
+        char temp = jumble[index1];
+        jumble[index1] = jumble[index2];
+        jumble[index2] = temp;
+
+    }
+
+    cout << "\t\t\tWelcome to Word Jumble!\n\n"; 
+    cout << "Unscramble the letters to make a word.\n"; 
+    cout << "Enter 'hint' for a hint.\n"; 
+    cout << "Enter 'quit' to quit the game.\n\n"; 
+    cout << "The jumble is: " << jumble;
+
+    string guess;
+    cout << "\n\nYour guess: ";
+    cin >> guess;
+
+    // Entering the Game Loop
+    while ((guess != theWord) && (guess != "quit"))
+    {
+        if (guess == "hint") {
+            cout << theHint;
+        }
+        else {
+            cout << "sorry, wrong word!\n";
+        }
+
+        cout << "\n\nYour guess: ";
+        cin >> guess;
+    }
+    if (guess == theWord)
+    {
+        cout << "\n That's it! Your guessed it!\n";
+    }
+
+    cout << "Thank you for playing!";
+
+
+    return 0;
+}
+
